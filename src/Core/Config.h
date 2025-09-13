7e884e6c85f85a21ccb5bb2d58845e46e2d31f02
Krixx <dev@krixx.anonaddy.com>
2025-09-13 13:48:17 +0200

    Release v0.4

--- src/Core/Config.h
+++ src/Core/Config.h
@@ -3,7 +3,7 @@
 #include <string_view> // For std::string_view
 
 namespace kx {
-    constexpr std::string_view APP_VERSION = "0.3";
+    constexpr std::string_view APP_VERSION = "0.4";
 
     // Configuration for the target process and function signature
     constexpr std::string_view TARGET_PROCESS_NAME = "Gw2-64.exe";
